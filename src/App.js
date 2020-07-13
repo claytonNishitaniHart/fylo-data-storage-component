504c29d16237a8874cb3f7ede0ddd2d149fedc1d
Clayton Nishitani-Hart <claytonnishitanihart@gmail.com>
2020-07-14 07:02:53 +1200

    finished desktop version

--- src/App.js
+++ src/App.js
@@ -1,57 +1,53 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import './App.scss';
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+import bg from "./images/bg-desktop.png";
+import logo from "./images/logo.svg";
+import doc from "./images/icon-document.svg";
+import folder from "./images/icon-folder.svg";
+import upload from "./images/icon-upload.svg";
+
+class App extends React.Component {
+  componentDidMount() {
+    window.addEventListener('load', this.handleLoad);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('load', this.handleLoad)
+  }
+  
+  handleLoad() {
+    document.getElementById("slider").style.width = "81.5%";
+  }
+  
+  render() {
+    return (
+      <div className={"App"}>
+        <img className={"App-Image"} src={bg} alt={"background"}/>
+        <div className={"Box-Primary"}>
+          <img className={"Box-Primary-Logo"} src={logo} alt={"logo"}/>
+          <button className={"Box-Primary-Button Box-Primary-Button1"}><img className={"Box-Primary-Button-Icon"} src={doc} alt={"document"}/></button>
+          <button className={"Box-Primary-Button Box-Primary-Button2"}><img className={"Box-Primary-Button-Icon"} src={folder} alt={"folder"}/></button>
+          <button className={"Box-Primary-Button Box-Primary-Button3"}><img className={"Box-Primary-Button-Icon"} src={upload} alt={"upload"}/></button>
+        </div>
+        <div className={"Box-Secondary"}>
+          <span className={"Box-Secondary-Text"}>You've used <span className={"Box-Secondary-Text-Bold"}>815 GB</span> of your storage.</span>
+          <div className={"Box-Secondary-Capacity"}>
+            <div id={"slider"} className={"Box-Secondary-Capacity-Slider"}/>
+            <span className={"Box-Secondary-Capacity-Min Box-Secondary-Text-Bold"}>
+              0 GB
+            </span>
+            <span className={"Box-Secondary-Capacity-Max Box-Secondary-Text-Bold"}>
+              1000 GB
+            </span>
+          </div>
+          <div className={"Box-Secondary-Remaining"}>
+            <span className={"Box-Secondary-Remaining-Text"}><span className={"Box-Secondary-Remaining-Text Box-Secondary-Remaining-Text-Bold"}>185 </span> GB LEFT</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
